0f1ae58fd3afb3ac3beb7347c7decc0996a23e54
k_czech <k.czech@rndevs.com>
2023-03-24 09:59:54 +0100

    test for tailwindcss

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pl">
       <Head />
-      <body>
+      <body className="bg-slate-800">
         <Main />
         <NextScript />
       </body>
